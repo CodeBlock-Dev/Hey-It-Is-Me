6908c79199b5848329bca47be84bdbe6ca76444a
hamed.shirbandi@gmail.com <hamed.shirbandi@gmail.com>
2025-12-26 01:07:24 +0100

    fix: pricing plan's order

--- src/1-Libraries/Application/Contracts/Constants.cs
+++ src/1-Libraries/Application/Contracts/Constants.cs
@@ -1,10 +1,13 @@
 ﻿namespace HeyItIsMe.Application.Contracts;
 
-internal class Constants
+public class Constants
 {
     public const string FACT_TEXT_GENERATOR_BOT = "Fact_Text_Generator_Bot";
     public const string FACT_TITLE_GENERATOR_PROMPT = "Fact_Title_Generator_Prompt";
     public const string FACT_CONTENT_GENERATOR_PROMPT = "Fact_Content_Generator_Prompt";
     public const string FACT_IMAGE_GENERATOR_BOT = "Fact_Image_Generator_Bot";
     public const string FACT_IMAGE_GENERATOR_PROMPT = "Fact_Image_Generator_Prompt";
+    public const string STARTER_PLAN = "Starter_Plan";
+    public const string ESSENTIAL_PLAN = "Essential_Plan";
+    public const string PREMIUM_PLAN = "Premium_Plan";
 }
